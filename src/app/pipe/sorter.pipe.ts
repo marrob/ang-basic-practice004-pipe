d318eebddd65bb4d5df7fd0e0eeb598fd27c0f76
Margit Robert <marrob50hz@gmail.com>
2021-01-28 07:44:39 +0100

    first

---
+++ src/app/pipe/sorter.pipe.ts
@@ -0,0 +1,51 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sorter'
+})
+export class SorterPipe implements PipeTransform {
+
+  /**
+   * A kapott tömb rendezése a szűrőkifejezés alapján.
+   * @param value {any[]} - a tömb.
+   * @param key {string} - az objektum kulcs, ami alapján rendez.
+   * @returns {any[]} - a kulcs alapján rendezett tömb.
+   */
+  transform(value: any[], key: string): any[] {
+    // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
+    //return value;
+
+    /**
+     * FELADAT!
+     * Ellenőrzés: ha a value nem tömb, vagy nincs megadva a key,
+     * térj vissza a value változóval.
+     */
+    if(!Array.isArray(value) || key == "")
+    return value;
+
+
+
+    /**
+     * FELADAT!
+     * Térj vissza a value.sort metódus eredményével!
+     * 1. Ha az a[key] és a b[key] típusa is szám, térj vissza a különbségükkel.
+     * 2. Ha nem számok, akkor mind a kettőt konvertáld string típusra,
+     *  azután alakítsd őket kisbetűssé.
+     * 3. Térj vissza a két string localeCompare metódus által visszaadott
+     *  összehasonlításának az eredményével.
+     */
+
+    return value.sort( (a, b)=> {
+        if(isNaN(a[key]) || isNaN(b[key])){
+            let a_str = a.toString().toLowerCase() ;
+            let b_str = b.toString().toLowerCase();
+            return a_str.localCompare(b_str);
+        }
+        else{
+            return a - b;
+        }
+    })
+
+  }
+
+}
